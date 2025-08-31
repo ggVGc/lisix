f19109ad8549e25be2be20b247bd02199cd50450
Valter Sundström <valter.sundstrom@gmail.com>
2025-08-31 05:45:03 +0200

    Remove temp test files

--- test_simple.exs
+++
@@ -1,5 +0,0 @@
-import Lisix.Sigil
-
-IO.puts("Testing basic sigil")
-result = ~L"(+ 1 2)"
-IO.puts("Result: #{result}")
--- test_tuples.exs
+++
@@ -1,6 +0,0 @@
-import Lisix.Sigil
-
-IO.puts("Testing tuple creation...")
-result = ~L"{:reply 42 42}"
-IO.puts("Tuple result: #{inspect(result)}")
-IO.puts("Done!")
